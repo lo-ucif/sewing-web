f2ae6c30aafdc5b2f19717f69fac25ef0a4b87a1
Loucif Tameur Ahmed <tameur_ahmed.loucif@univ-constantine2.dz>
2026-05-20 20:57:49 +0100

    feat: update routing to use GalleryPage and ProjectDetailPage components

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import ProjectDetails from './pages/ProjectDetails';
-import Categories from './pages/Categories';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import GalleryPage from "./pages/GalleryPage";
+import ProjectDetailPage from "./pages/ProjectDetailPage";
 
 export const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/projects/:id" element={<ProjectDetails />} />
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route path="/projects" element={<GalleryPage />} />
+      <Route path="/projects/:id" element={<ProjectDetailPage />} />
+      <Route path="/categories" element={<Home />} />
+      <Route path="/about" element={<Home />} />
+      <Route path="/contact" element={<Home />} />
       {/* Fallback to Home */}
       <Route path="*" element={<Home />} />
     </Routes>
